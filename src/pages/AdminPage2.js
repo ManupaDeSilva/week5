ad66b16ca529226c96c228e45f742f454899120b
Manu♛ Bimsara <s18340@sci.pdn.ac.lk>
2023-12-15 22:36:21 +0530

    week5

---
+++ src/pages/AdminPage2.js
@@ -0,0 +1,79 @@
+import "./AdminPage2.css";
+
+const AdminPage2 = () => {
+  return (
+    <div className="admin-page2">
+      <div className="admin-page2-child" />
+      <div className="last-name">Last Name</div>
+      <div className="gender">Gender</div>
+      <div className="contact">Contact</div>
+      <div className="address">Address</div>
+      <div className="email">Email</div>
+      <div className="password">Password</div>
+      <div className="faculty">Faculty</div>
+      <div className="which-year-student">Which Year Student</div>
+      <div className="user-id">User Id</div>
+      <div className="registration-number">Registration number</div>
+      <div className="which-sports-and">{`Which Sports and Practicess You are attendent in Gymnasium `}</div>
+      <div className="mension-the-sports">{`Mension the Sports, which you are a member of `}</div>
+      <div className="admin-page2-item" />
+      <div className="first-name">First Name</div>
+      <div className="kamal-wrapper">
+        <div className="kamal">{`Kamal `}</div>
+      </div>
+      <div className="no55horana126roadcolombo-wrapper">
+        <div className="kamal">No.55,Horana,126Road,Colombo</div>
+      </div>
+      <div className="s18554scipdnaclk-wrapper">
+        <div className="kamal">s18554@sci.pdn.ac.lk</div>
+      </div>
+      <div className="wrapper">
+        <div className="kamal">12345</div>
+      </div>
+      <div className="rd-year-wrapper">
+        <div className="kamal">3rd year</div>
+      </div>
+      <div className="s18554-wrapper">
+        <div className="kamal">S18554</div>
+      </div>
+      <div className="container">
+        <div className="kamal">19973625349</div>
+      </div>
+      <div className="badminton-karate-wrapper">
+        <div className="badminton-karate">Badminton , Karate</div>
+      </div>
+      <div className="badminton-wrapper">
+        <div className="badminton">{`Badminton `}</div>
+      </div>
+      <div className="science-wrapper">
+        <div className="kamal">Science</div>
+      </div>
+      <div className="jayasinghe-wrapper">
+        <div className="kamal">Jayasinghe</div>
+      </div>
+      <div className="hmkamal-jayasingha-wrapper">
+        <div className="hmkamal-jayasingha">H.M.KAMAL JAYASINGHA</div>
+      </div>
+      <div className="male-wrapper">
+        <div className="kamal">Male</div>
+      </div>
+      <div className="frame">
+        <div className="kamal">076823654</div>
+      </div>
+      <div className="admin-page2-inner" />
+      <div className="rectangle-div" />
+      <div className="frame-div">
+        <div className="facility-booking-details-wrapper">
+          <div className="kamal">Facility Booking Details</div>
+        </div>
+      </div>
+      <div className="equipment-booking-details">Equipment Booking Details</div>
+      <div className="admin-page2-child1" />
+      <div className="admin-page2-child2" />
+      <div className="view">{`View `}</div>
+      <div className="view1">{`View `}</div>
+    </div>
+  );
+};
+
+export default AdminPage2;
---
+++ src/pages/IndoorFacilities.js
@@ -0,0 +1,266 @@
+import "./IndoorFacilities.css";
+
+const IndoorFacilities = () => {
+  return (
+    <div className="indoorfacilities">
+      <img
+        className="indoorfacilityimg-icon"
+        alt=""
+        src="/indoorfacilityimg@2x.png"
+      />
+      <div className="header">
+        <div className="department-of-physical-container">
+          <p className="department-of-physical">
+            Department of Physical Education
+          </p>
+          <p className="university-of-peradeniya">University of Peradeniya</p>
+        </div>
+        <div className="headerback" />
+        <img className="unilogo-icon" alt="" src="/unilogo@2x.png" />
+        <div className="logout">
+          <b className="log-out">Log out</b>
+        </div>
+        <div className="navigationbar">
+          <div className="naviback" />
+          <button className="home">Home</button>
+          <button className="about-us">About Us</button>
+          <button className="registration">Registration</button>
+          <button className="payments">Payments</button>
+          <button className="equipments">Equipments</button>
+          <button className="sports">Sports</button>
+          <button className="locker-system">Locker System</button>
+          <button className="facilities">Facilities</button>
+          <button className="daily-events">Daily Events</button>
+          <button className="classes">Classes</button>
+          <button className="special-programmes">Special Programmes</button>
+        </div>
+        <img className="uopname-icon" alt="" src="/uopname@2x.png" />
+      </div>
+      <div className="footer">
+        <img className="mapimage-icon" alt="" src="/mapimage@2x.png" />
+        <div className="contactus">
+          <div className="contact-us-departments-container">
+            <p className="contact-us">
+              <b>Contact Us</b>
+            </p>
+            <p className="blank-line">
+              <b>
+                <span>&nbsp;</span>
+              </b>
+            </p>
+            <p className="departments-of-physical-educat">
+              <b>
+                <span>Departments of Physical Education,</span>
+              </b>
+            </p>
+            <p className="peradeniya">
+              <b>
+                <span>University of Peradeniya,</span>
+              </b>
+            </p>
+            <p className="peradeniya">
+              <b>
+                <span>Peradeniya</span>
+              </b>
+            </p>
+            <p className="peradeniya">
+              <b>
+                <span>&nbsp;</span>
+              </b>
+            </p>
+            <p className="telephone">
+              <b>Telephone</b>
+            </p>
+            <p className="blank-line2">
+              <b>&nbsp;</b>
+            </p>
+            <p className="peradeniya">
+              <b>Office - 081 - 239 2162</b>
+            </p>
+            <p className="director-081-239-2164">
+              <b>Director - 081 - 239 2164</b>
+            </p>
+          </div>
+          <img className="callicon" alt="" src="/callicon@2x.png" />
+          <img className="locationicon" alt="" src="/locationicon@2x.png" />
+        </div>
+      </div>
+      <div className="maintopic">
+        <b className="indoor-facilities">INDOOR FACILITIES</b>
+      </div>
+      <div className="in-the-university">In the University Gymnasium</div>
+      <div className="university-gymnasium-is">{`University Gymnasium is a large indoor complex equipped with top –class sports training facilities. This huge building offers facilities for badminton, basketball, netball, volleyball, 5-a-side football (futsal), trampoline, aerobics, carom, chess, table tennis, weight lifting, powerlifting, weight training, wushu, karate, taekwondo, judo, kick boxing & wrestling. The playing area is covered with state-of –the-art high quality wooden floor. It is provided with 04 large tilled wash rooms & changing rooms plus a balcony for the spectators.`}</div>
+      <div className="posseses">
+        <b className="gymnasium-possesses">Gymnasium Possesses</b>
+        <img className="line-icon" alt="" src="/line@2x.png" />
+        <b className="badminton-courts">Badminton courts</b>
+        <b className="basketball-court">Basketball court</b>
+        <b className="netball-court">Netball court</b>
+        <b className="volleyball-courts">Volleyball courts</b>
+        <b className="table-tennis-tables">Table Tennis Tables</b>
+        <b className="taekwondo-area">Taekwondo area</b>
+        <b className="wrestling-area">Wrestling area</b>
+        <b className="chess-carom">{`Chess & Carom rooms`}</b>
+        <b className="climbing-board">Climbing board</b>
+        <b className="specialist-fitness-centre">{`Specialist fitness Centre equipped for weight training & Fitness`}</b>
+        <b className="trampoline">Trampoline</b>
+        <img className="dot-icon" alt="" src="/dot@2x.png" />
+        <img className="dot-icon1" alt="" src="/dot@2x.png" />
+        <img className="dot-icon2" alt="" src="/dot@2x.png" />
+        <img className="dot-icon3" alt="" src="/dot@2x.png" />
+        <img className="dot-icon4" alt="" src="/dot@2x.png" />
+        <img className="dot-icon5" alt="" src="/dot@2x.png" />
+        <img className="dot-icon6" alt="" src="/dot@2x.png" />
+        <img className="dot-icon7" alt="" src="/dot@2x.png" />
+        <img className="dot-icon8" alt="" src="/dot@2x.png" />
+        <img className="dot-icon9" alt="" src="/dot@2x.png" />
+        <img className="dot-icon10" alt="" src="/dot@2x.png" />
+      </div>
+      <div className="badmintoncourt">
+        <b className="badmintoncourt1">Badminton Courts</b>
+        <div className="wrapper-badmintonimg">
+          <img
+            className="badmintonimg-icon"
+            alt=""
+            src="/badmintonimg@2x.png"
+          />
+        </div>
+        <button className="book">
+          <div className="book1">BOOK</div>
+        </button>
+      </div>
+      <div className="bascketballcourt">
+        <b className="bascketballcourt1">Bascketball Court</b>
+        <div className="wrapper-badmintonimg">
+          <img
+            className="badmintonimg-icon"
+            alt=""
+            src="/bascketballcourtimg@2x.png"
+          />
+        </div>
+        <button className="book">
+          <div className="book1">BOOK</div>
+        </button>
+      </div>
+      <div className="volleyballcourt">
+        <b className="volleyballcourt1">Volleyball Courts</b>
+        <div className="wrapper-badmintonimg">
+          <img
+            className="badmintonimg-icon"
+            alt=""
+            src="/volleyballcourtimg@2x.png"
+          />
+        </div>
+        <button className="book">
+          <div className="book1">BOOK</div>
+        </button>
+      </div>
+      <div className="netballcourt">
+        <b className="netballcourt1">Netball Court</b>
+        <div className="wrapper-badmintonimg">
+          <img
+            className="badmintonimg-icon"
+            alt=""
+            src="/netballcourtimg@2x.png"
+          />
+        </div>
+        <button className="book">
+          <div className="book1">BOOK</div>
+        </button>
+      </div>
+      <div className="tabletenniscourt">
+        <b className="tabletenniscourt1">TableTennis Courts</b>
+        <div className="wrapper-badmintonimg">
+          <img
+            className="badmintonimg-icon"
+            alt=""
+            src="/tabletenniscourtimg@2x.png"
+          />
+        </div>
+        <button className="book">
+          <div className="book1">BOOK</div>
+        </button>
+      </div>
+      <div className="taekwondoarea">
+        <b className="taekwondoarea1">Taekwondo area</b>
+        <div className="wrapper-badmintonimg">
+          <img
+            className="badmintonimg-icon"
+            alt=""
+            src="/taekwondoareaimg@2x.png"
+          />
+        </div>
+        <button className="book">
+          <div className="book1">BOOK</div>
+        </button>
+      </div>
+      <div className="wrestlingarea">
+        <b className="wrestlingarea1">Wrestling area</b>
+        <div className="wrapper-badmintonimg">
+          <img
+            className="badmintonimg-icon"
+            alt=""
+            src="/wrestlingareaimg@2x.png"
+          />
+        </div>
+        <button className="book">
+          <div className="book1">BOOK</div>
+        </button>
+      </div>
+      <div className="fitnesscentre">
+        <b className="fitnesscentre1">{`Specialist fitness Centre equipped for weight training & Fitness`}</b>
+        <div className="wrapper-badmintonimg">
+          <img
+            className="badmintonimg-icon"
+            alt=""
+            src="/fitnesscentreimg@2x.png"
+          />
+        </div>
+        <button className="book">
+          <div className="book1">BOOK</div>
+        </button>
+      </div>
+      <div className="trampoline1">
+        <b className="trampoline2">Trampoline</b>
+        <div className="wrapper-badmintonimg">
+          <img
+            className="badmintonimg-icon"
+            alt=""
+            src="/trampolineimg@2x.png"
+          />
+        </div>
+        <button className="book">
+          <div className="book1">BOOK</div>
+        </button>
+      </div>
+      <div className="chesscaromrooms">
+        <b className="chesscaromrooms1">{`Chess & Carom rooms`}</b>
+        <div className="wrapper-badmintonimg">
+          <img
+            className="badmintonimg-icon"
+            alt=""
+            src="/chesscaromroomsimg@2x.png"
+          />
+        </div>
+        <button className="book">
+          <div className="book1">BOOK</div>
+        </button>
+      </div>
+      <div className="climbingboard">
+        <b className="climbingboard1">Climbing Board</b>
+        <div className="wrapper-climbingboardimg">
+          <img
+            className="climbingboardimg-icon"
+            alt=""
+            src="/climbingboardimg@2x.png"
+          />
+        </div>
+        <button className="book20">
+          <div className="book1">BOOK</div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default IndoorFacilities;
